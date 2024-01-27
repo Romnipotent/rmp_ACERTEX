bc76bfe9f289fabe02875db0ee2b5e80e8e9da83
Romnipotent <148759053+Romnipotent@users.noreply.github.com>
2024-01-27 22:49:15 +1100

    Add files via upload

---
+++ mod.cpp
@@ -0,0 +1,8 @@
+name = "rmp_ACE_RTEX";
+picture = "RMP_CA.paa";
+description = "Vehicle Maintenance Enhanced";
+logo = "RMP_CA.paa";
+logoOver = "RMP_CA.paa";
+tooltip = "RTEX";
+author = "Romnipotent";
+overviewPicture = "RMP_CA.paa";
